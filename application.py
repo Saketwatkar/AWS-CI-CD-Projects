fb0dcfbed23fcbb4feedea90ec94714e694e8216
saket watkar <147791587+Saketwatkar@users.noreply.github.com>
2025-07-15 19:54:46 +0530

    Update application.py

--- application.py
+++ application.py
@@ -2,6 +2,8 @@ from flask import Flask, request, render_template
 import numpy as np
 import pandas as pd
 import traceback
+
+
 from sklearn.preprocessing import StandardScaler
 from src.pipeline.predict_pipeline import CustomData, PredictPipeline
 
